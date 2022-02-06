e8b569e9c86c053a77c7503aa6396102aef9c4f7
jonnebonde <jonnebonde@gmail.com>
2022-02-06 17:48:41 +0100

    enter

--- js/script.js
+++ js/script.js
@@ -20,3 +20,68 @@ button.onclick = function () {
 
     document.bgColor = "grey";
 };
+
+
+// Data attributes and adding onclick functions in a loop
+
+/* const one  = document.querySelector(".one"); */
+
+
+// Access different dataset in the html.
+/* one.onclick = function() {
+    console.log(event.target.dataset.price);
+} */
+
+/* one.onclick = function() {
+    console.log(event.target.dataset.colour);
+} */
+
+
+// Looping through the datasets and adding onclick function to every div.
+
+
+const divs = document.querySelectorAll(".container > div");
+
+for(let i = 0;i < divs.length; i++) {
+    console.log(divs[i].className);
+
+    divs[i].onclick = function() {
+        console.log(event.target.dataset.price);
+    }
+;}
+
+
+// onmouseover and onmouseout events
+
+
+const hoverDiv = document.querySelector(".hover-test");
+
+/* hoverDiv.onmouseover = function() {
+        console.log("hovering over");
+
+        event.target.classList.add("hover");
+};
+
+hoverDiv.onmouseout = function() {
+    console.log("hovering stops");
+
+    event.target.classList.remove("hover");
+}; */
+
+
+const divsMouse = document.querySelectorAll(".containerevent > div");
+const display = document.querySelector(".display");
+
+for(let i = 0; i < divsMouse.length; i++) {
+    divsMouse[i].onmouseover = function() {
+
+        const content = event.target.dataset.content;
+        display.innerHTML = content;
+
+    }
+
+    divsMouse[i].onmouseout = function() {
+        display.innerHTML = "";
+    }
+
+}
